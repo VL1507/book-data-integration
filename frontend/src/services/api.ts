ed57f78390852e6f8f24ceedcfdad7401f6e9cae
VL1507 <vova.mashyanov@gmail.com>
2026-01-05 00:03:21 +1000

    .

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import type { Book, BookFilters } from '@/types/book'
 
+// TODO: добавить путь в env
 const API_BASE_URL = 'http://localhost:8000'
 
 const api = axios.create({
