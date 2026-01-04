86b93c264a057374ea6da48e203e9801031a5e86
VL1507 <vova.mashyanov@gmail.com>
2026-01-05 00:03:15 +1000

    .

--- backend/main.py
+++ backend/main.py
@@ -110,5 +110,7 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
+    
+    print("\n"*10)
 
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
