1232c2c9cfd205f3675b225dea85c50095c886ef
VL1507 <vova.mashyanov@gmail.com>
2026-01-04 19:32:54 +1000

    add custom_settings

--- book_sites_crawler/book_sites_crawler/spiders/chitai_gorod.py
+++ book_sites_crawler/book_sites_crawler/spiders/chitai_gorod.py
@@ -11,6 +11,21 @@ class ChitaiGorodSpider(Spider):
     allowed_domains = ["chitai-gorod.ru"]
     start_urls = ["https://chitai-gorod.ru/catalog/books-18030"]
 
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0",
+        "CONCURRENT_REQUESTS": 16,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 8,
+        #
+        "DOWNLOAD_DELAY": 0.5,
+        "RANDOMIZE_DOWNLOAD_DELAY": True,
+        "DOWNLOAD_TIMEOUT": 30,
+        #
+        "HTTPCACHE_ENABLED": True,
+        #
+        # "LOG_LEVEL": "INFO",
+    }
+
     def parse(self, response: Response, **kwargs: Any) -> Any:
         book_links = response.css("a.product-card__title::attr(href)").getall()
         print(f"{len(book_links)=}")
