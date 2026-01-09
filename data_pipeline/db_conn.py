0599ed9f88f12186c1042536b01e4e5d1bbcaca1
VL1507 <vova.mashyanov@gmail.com>
2026-01-09 21:51:47 +1000

    gg

--- data_pipeline/db_conn.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from config import DATABASE_URL
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine(DATABASE_URL, echo=False)
-
-
-Session = sessionmaker(bind=engine)
