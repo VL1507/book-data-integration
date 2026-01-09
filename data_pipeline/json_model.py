2a11d424f6d7fd6136fe9aa8ca90544d4d1d98bf
VL1507 <vova.mashyanov@gmail.com>
2026-01-09 18:05:39 +1000

    add field

--- data_pipeline/json_model.py
+++ data_pipeline/json_model.py
@@ -1,39 +1,41 @@
 from dataclasses import dataclass
 
+from dataclasses import field
+
 
 @dataclass
 class BookSitesCrawlerItem:
-    isbn: list[str]
-    year: int | None
-    page_count: int | None
-    dim_x: int | None
-    dim_y: int | None
-    dim_z: int | None
+    isbn: list[str] = field(default_factory=list)
+    year: int | None = field(default=None)
+    page_count: int | None = field(default=None)
+    dim_x: int | None = field(default=None)
+    dim_y: int | None = field(default=None)
+    dim_z: int | None = field(default=None)
 
-    books_name: str | None
+    books_name: str | None = field(default=None)
 
-    authors_name: list[str]
+    authors_name: list[str] = field(default_factory=list)
 
-    sites_site: str
-    sites_url: str
+    sites_site: str = field(default="")
+    sites_url: str = field(default="")
 
-    publication_site_price: float | None
+    publication_site_price: float | None = field(default=None)
 
-    lang: str | None
+    lang: str | None = field(default=None)
 
-    description: str | None
+    description: str | None = field(default=None)
 
-    publishing_houses_name: str | None
-    publishing_houses_url: str | None
+    publishing_houses_name: str | None = field(default=None)
+    publishing_houses_url: str | None = field(default=None)
 
-    recension_link: str | None
+    recension_link: str | None = field(default=None)
 
-    illustration_types_name: str | None
+    illustration_types_name: str | None = field(default=None)
 
-    coverages_types_name: str | None
+    coverages_types_name: str | None = field(default=None)
 
-    image_urls: str | None
+    image_urls: str | None = field(default=None)
 
-    url: str | None
+    url: str | None = field(default=None)
 
-    genre: str | None
+    genre: list[str] = field(default_factory=list)
