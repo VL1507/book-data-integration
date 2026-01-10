d28827215421b5a2e385338cee4f250ce07387e6
VL1507 <vova.mashyanov@gmail.com>
2026-01-10 20:04:36 +1000

    style: format imports

--- data_pipeline/src/db_conn.py
+++ data_pipeline/src/db_conn.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
-from config import DATABASE_URL
 from sqlalchemy.orm import sessionmaker
+
+from config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, echo=False)
 
