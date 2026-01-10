3655065fdeaa48fd5ef32ee3f720376696bb1571
VL1507 <vova.mashyanov@gmail.com>
2026-01-10 22:41:19 +1000

    add timer

---
+++ data_pipeline/src/wrap_timer.py
@@ -0,0 +1,17 @@
+import time
+import functools
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(
+            f"Функция {func.__name__!r} --- {total_time // 60} мин {total_time % 60:.4f} сек "
+        )
+        return result
+
+    return wrapper_timer
