736db786a86ab8d604002a5ca227630075341c9c
VL1507 <vova.mashyanov@gmail.com>
2026-01-11 15:31:54 +1000

    json path to raw string

--- data_pipeline/src/config.py
+++ data_pipeline/src/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-JSON_PATH_BOOK24 = Path("results\book24.json")
-JSON_PATH_CHITAI_GOROD = Path("results\chitai-gorod.json")
-JSON_PATH_LABIRINT = Path("results\labirint\.json")
+JSON_PATH_BOOK24 = Path(r"results\book24.json")
+JSON_PATH_CHITAI_GOROD = Path(r"results\chitai-gorod.json")
+JSON_PATH_LABIRINT = Path(r"results\labirint\.json")
 print(DATABASE_URL)
