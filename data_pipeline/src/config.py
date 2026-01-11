a4aec08aa9b3fefb72e85461f4c6d63045af7485
VL1507 <vova.mashyanov@gmail.com>
2026-01-11 15:30:31 +1000

    edit json path

--- data_pipeline/src/config.py
+++ data_pipeline/src/config.py
@@ -8,11 +8,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-JSON_PATH_BOOK24 = Path("./results/json/book24/book24_2026-01-10T15-56-20+00-00.json")
-JSON_PATH_CHITAI_GOROD = Path(
-    "results\json\chitai-gorod\chitai-gorod_2026-01-10T15-58-38+00-00.json"
-)
-JSON_PATH_LABIRINT = Path(
-    "results\json\labirint\labirint_2026-01-10T16-01-38+00-00.json"
-)
+JSON_PATH_BOOK24 = Path("results\book24.json")
+JSON_PATH_CHITAI_GOROD = Path("results\chitai-gorod.json")
+JSON_PATH_LABIRINT = Path("results\labirint\.json")
 print(DATABASE_URL)
