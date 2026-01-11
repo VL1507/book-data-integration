d744ab1749c1c80be42d364351b610b059290d53
endermend <endermend05@gmail.com>
2026-01-11 13:32:12 +1000

    Starting updating book data model

--- backend/src/data/books_data.py
+++ backend/src/data/books_data.py
@@ -1,14 +1,53 @@
 from pydantic import BaseModel
-
+from typing import List
 
 class Book(BaseModel):
     id: int
     title: str
     author: str
     year: int
-    genre: str
+    genre: List[str]
     description: str
-    image_filename: str
+    image_url: str
+
+class Publication(BaseModel):
+    pass
+
+class BookSitesCrawlerItem(BaseModel):
+    isbn: list[str] = field(default_factory=list)
+    year: int | None = field(default=None)
+    page_count: int | None = field(default=None)
+    dim_x: int | None = field(default=None)
+    dim_y: int | None = field(default=None)
+    dim_z: int | None = field(default=None)
+
+    books_name: str | None = field(default=None)
+
+    authors_name: list[str] = field(default_factory=list)
+
+    sites_site: str = field(default="")
+    sites_url: str = field(default="")
+
+    publication_site_price: float | None = field(default=None)
+
+    lang: str | None = field(default=None)
+
+    description: str | None = field(default=None)
+
+    publishing_houses_name: str | None = field(default=None)
+    publishing_houses_url: str | None = field(default=None)
+
+    recension_link: str | None = field(default=None)
+
+    illustration_types_name: str | None = field(default=None)
+
+    coverages_types_name: str | None = field(default=None)
+
+    image_urls: str | None = field(default=None)
+
+    url: str | None = field(default=None)
+
+    genre: list[str] = field(default_factory=list)
 
 
 # База данных в виде списка книг
