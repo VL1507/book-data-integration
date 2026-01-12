2626b7302b2ebef132b22ebced4537e94461cb71
VL1507 <vova.mashyanov@gmail.com>
2026-01-12 19:54:13 +1000

    add book_service in deps

--- backend/src/handlers/book.py
+++ backend/src/handlers/book.py
@@ -1,61 +1,29 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from pydantic import BaseModel
-
+from dependency import get_book_service
+from service.book import BookService
 
 router = APIRouter(prefix="/books")
 
 
 @router.get("/{isbn:str}")
-async def get_book_by_isbn(isbn: str):
+async def get_book_by_isbn(
+    isbn: str,
+    book_service: BookService = Depends(get_book_service),
+):
     print(isbn)
-    return isbn
+    book = await book_service.get_book_by_isbn(isbn=isbn)
+    return book
 
 
 @router.get("/")
-async def get_books():
-    print(get_books)
-    return
-
-
-# @app.get("/{book_id}", response_model=Book)
-# async def get_book(book_id: int):
-#     """Получить информацию о книге по её ID"""
-#     book = next((book for book in books_db if book.id == book_id), None)
-#     if book is None:
-#         raise HTTPException(status_code=404, detail="Book not found")
-#     return book
-
-
-# @app.get("/", response_model=List[Book])
-# async def get_books(
-#     genre: Optional[str] = Query(None, description="Фильтр по жанру"),
-#     author: Optional[str] = Query(None, description="Фильтр по автору"),
-#     year_from: Optional[int] = Query(None, description="Год от", ge=1000, le=2100),
-#     year_to: Optional[int] = Query(None, description="Год до", ge=1000, le=2100),
-#     limit: int = Query(10, description="Лимит результатов", ge=1, le=100),
-#     offset: int = Query(0, description="Смещение", ge=0),
-# ):
-#     """Получить список книг с фильтрацией и пагинацией"""
-#     filtered_books = books_db.copy()
-
-#     if genre:
-#         filtered_books = [
-#             book for book in filtered_books if genre.lower() in book.genre.lower()
-#         ]
-
-#     if author:
-#         filtered_books = [
-#             book for book in filtered_books if author.lower() in book.author.lower()
-#         ]
-
-#     if year_from:
-#         filtered_books = [book for book in filtered_books if book.year >= year_from]
-
-#     if year_to:
-#         filtered_books = [book for book in filtered_books if book.year <= year_to]
-
-#     start_index = offset
-#     end_index = offset + limit
-#     paginated_books = filtered_books[start_index:end_index]
-
-#     return paginated_books
+async def get_books(
+    genre: str | None = Query(None, description="Фильтр по жанру"),
+    author: str | None = Query(None, description="Фильтр по автору"),
+    year_from: int | None = Query(None, description="Год от", ge=1000, le=2100),
+    year_to: int | None = Query(None, description="Год до", ge=1000, le=2100),
+    limit: int = Query(10, description="Лимит результатов", ge=1, le=100),
+    offset: int = Query(0, description="Смещение", ge=0),
+    book_service: BookService = Depends(get_book_service),
+):
+    print(genre, author, year_from, year_to, limit, offset)
+    return [genre, author, year_from, year_to, limit, offset]
