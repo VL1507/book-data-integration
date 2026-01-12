ed8f54da1d92ce0d0e3335662db19a08841dfbae
VL1507 <vova.mashyanov@gmail.com>
2026-01-12 18:40:31 +1000

    add health router

--- backend/src/handlers/__init__.py
+++ backend/src/handlers/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from . import book
+from . import book, health
 
 api_router = APIRouter()
 
 api_router.include_router(book.router)
+api_router.include_router(health.router)
