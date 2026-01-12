f0a2522aa865927dfbbafdee6dc35f2148bffd88
VL1507 <vova.mashyanov@gmail.com>
2026-01-12 19:55:55 +1000

    fix bug with repository name

--- backend/src/service/book.py
+++ backend/src/service/book.py
@@ -5,7 +5,7 @@ from repository.book import BookRepository
 
 @dataclass
 class BookService:
-    user_repository: BookRepository
+    book_repository: BookRepository
 
     async def get_book_by_isbn(self, isbn: str):
         pass
