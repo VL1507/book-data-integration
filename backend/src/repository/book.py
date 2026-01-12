284469a9532c4a541c66d0969c5aa960025aa76a
VL1507 <vova.mashyanov@gmail.com>
2026-01-12 22:49:12 +1000

    set var Private

--- backend/src/repository/book.py
+++ backend/src/repository/book.py
@@ -23,9 +23,20 @@ from database.models import (
 )
 
 
-@dataclass
 class BookRepository:
-    db_session: AsyncSession
+    def __init__(self, db_session: AsyncSession):
+        self.__db_session = db_session
 
-    async def get_book_by_isbn(self, isbn: str):
+    async def get_book_by_publication_id(self, publication_id: int):
+        pass
+
+    async def get_books(
+        self,
+        limit: int = 100,
+        offset: int = 0,
+        genre: str | None = None,
+        author: str | None = None,
+        year_from: int | None = None,
+        year_to: int | None = None,
+    ):
         pass
--- backend/src/service/book.py
+++ backend/src/service/book.py
@@ -1,15 +1,29 @@
-from dataclasses import dataclass
-
 from repository.book import BookRepository
+from schema.book import BookForListSchema
 
 
-@dataclass
 class BookService:
-    book_repository: BookRepository
+    def __init__(self, book_repository: BookRepository):
+        self.__book_repository = book_repository
 
-    async def get_book_by_isbn(self, isbn: str):
-        result = await self.book_repository.get_book_by_isbn(isbn=isbn)
+    async def get_book_by_publication_id(self, publication_id: str):
+        result = await self.__book_repository.get_book_by_pu(
+            publication_id=publication_id
+        )
         if result is None:
             return None
         book = ...
         return book
+
+    async def get_books(
+        self,
+        limit: int = 100,
+        offset: int = 0,
+        genre: str | None = None,
+        author: str | None = None,
+        year_from: int | None = None,
+        year_to: int | None = None,
+    ) -> list[BookForListSchema]:
+        result = await self.__book_repository.get_books()
+        books: list[BookForListSchema] = []
+        return books
