7fd67e50c87dc29b78f242c4c24f986ee26df785
endermend <endermend05@gmail.com>
2026-01-12 18:47:52 +1000

    Updated clear logic

--- deduplicator/src/clear.py
+++ deduplicator/src/clear.py
@@ -12,7 +12,7 @@ def process_word(word):
     :param word: Слово, которое необходимо преобразовать
     :return: Очищенная версия слова
     '''
-    bad_words = ['Манга', 'Новелла', 'Манхва', 'Подарочное издание', 'Официальный мерч', 'Тату', 'Роман', 'Артбук', 'Сборник', 'Повесть', '"', '«', '»', "'"]
+    bad_words = ['Манга', 'Новелла', 'Манхва', 'Подарочное издание', 'Официальный мерч', 'Тату', 'Роман', 'Артбук', 'Сборник', 'Повесть', 'Дунхуа', '"', '«', '»', "'"]
     bad_words.extend([x.lower() for x in bad_words])
     word = re.sub(r'\(#([1-9])\)', lambda m: f'№{m.group(1)}', word)
     word = re.sub(r'\((?!Не\))(?!не\))[^)]*\)', '', word)
@@ -20,7 +20,7 @@ def process_word(word):
     word = re.sub(r'\+[^$]*', '', word)
     for b in bad_words:
         word = word.replace(b, '')
-    while '..' in word or '  ' in word:
+    while '..' in word or '  ' in word or ' .' in word:
         word = re.sub(r'\s{2,}', ' ', word)
         word = re.sub(r'(\s\.)|(\.{2,})', '.', word)
     word = word.strip(' .,|:;-')
