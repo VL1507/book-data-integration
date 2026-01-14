046dab94f6a382deac0721b917ad6b83fe10fdb8
VL1507 <vova.mashyanov@gmail.com>
2026-01-14 17:41:19 +1000

    add nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,13 +11,17 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: process.env.VITE_BACKEND_URL,
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
+  base: '/', // важно оставить / (или /subpath/ если будет подкаталог)
+  build: {
+    outDir: 'dist',
   },
 })
